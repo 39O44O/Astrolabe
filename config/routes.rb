091261bbd56ddd43e523cb0eb480c6a095c5c416
39O44O <yoshio.sakuma.engineer@gmail.com>
2021-01-05 14:00:50 +0000

    ゲストログイン機能 実装 #1

--- config/routes.rb
+++ config/routes.rb
@@ -28,4 +28,9 @@ Rails.application.routes.draw do
   
   resources :users, only: [:show]
   
+  # ゲストログイン機能
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'sessions#new_guest'
+  end
+  
 end
